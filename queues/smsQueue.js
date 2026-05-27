63a8972a80619dd2ba551f9a70157377ab8b101b
Subhra Mukherjee <subhra.m@techmantra.co>
2026-05-27 18:59:22 +0530

    push

--- queues/smsQueue.js
+++ queues/smsQueue.js
@@ -1,15 +1,19 @@
 // queues/smsQueue.js
-const { Queue } = require("bullmq");
-const connection = require("../redisConnection");
+// const { Queue } = require("bullmq");
+// const connection = require("../redisConnection");
 
-const smsQueue = new Queue("sms", {
-  connection,
-  defaultJobOptions: {
-    attempts: 5,
-    backoff: { type: "exponential", delay: 2000 },
-    removeOnComplete: true,
-    removeOnFail: false,
-  },
-});
+// const smsQueue = new Queue("sms", {
+//   connection,
+//   defaultJobOptions: {
+//     attempts: 5,
+//     backoff: { type: "exponential", delay: 2000 },
+//     removeOnComplete: true,
+//     removeOnFail: false,
+//   },
+// });
+
+const smsQueue = {
+  add: async () => null,
+};
 
 module.exports = smsQueue;
