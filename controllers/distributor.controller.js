8f36d58c7d119237f603ad41de0f134103fcefde
Suvo Biswas <suvo@techmantra.co>
2026-06-01 15:03:32 +0530

    okk

--- controllers/distributor.controller.js
+++ controllers/distributor.controller.js
@@ -326,7 +326,6 @@ const disList = asyncHandler(async (req, res) => {
 
 const updateDistributor = asyncHandler(async (req, res) => {
   const { _id } = req.body;
-  
 
   const distributor = await Distributor.findById(_id);
 
