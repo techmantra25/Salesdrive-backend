af95cdefd449030d2567ad4fbecb4c8552e3eec4
Suvo Biswas <suvo@techmantra.co>
2026-08-06 10:46:42 +0530

    okk

--- models/bill.model.js
+++ models/bill.model.js
@@ -171,6 +171,10 @@ const BillSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "Employee",
     },
+    cso: {
+      type: String,
+      default: null,
+    },
     routeId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Beat",
