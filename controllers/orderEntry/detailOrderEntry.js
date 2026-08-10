f6e6a6b45ad1bdf05696e19e2c51bd0e7369bce7
Suvo Biswas <suvo@techmantra.co>
2026-08-10 13:22:31 +0530

    beat show

--- controllers/orderEntry/detailOrderEntry.js
+++ controllers/orderEntry/detailOrderEntry.js
@@ -1,79 +1,96 @@
 const asyncHandler = require("express-async-handler");
 const OrderEntry = require("../../models/orderEntry.model");
+const Beat = require("../../models/beat.model");
 
 // Get Order Entry Details by ID
 const detailOrderEntry = asyncHandler(async (req, res) => {
-  try {
-    const orderEntry = await OrderEntry.findById(req.params.id).populate([
-      {
-        path: "distributorId",
-        select: "",
-      },
-      {
-        path: "salesmanName",
-        select: "",
-      },
-      {
-        path: "routeId",
-        select: "",
-      },
-      {
-        path: "retailerId",
-        select: "",
-        populate: [
-          {
-            path: "stateId",
-            select: "",
-            populate: {
-              path: "zoneId",
-              select: "",
-            },
-          },
-          {
-            path: "regionId",
-            select: "",
-          },
-          {
-            path: "beatId",
-            select: "",
-          },
-        ],
-      },
-      {
-        path: "lineItems.product",
-        select: "",
-      },
-      {
-        path: "lineItems.price",
-        select: "",
-      },
-      {
-        path: "lineItems.inventoryId",
-        select: "",
-      },
-      { path: "billIds", select: "" },
-    ]);
+try {
+const orderEntry = await OrderEntry.findById(req.params.id).populate([
+{
+path: "distributorId",
+select: "",
+},
+{
+path: "salesmanName",
+select: "",
+},
+{
+path: "routeId",
+select: "",
+},
+{
+path: "retailerId",
+select: "",
+populate: [
+{
+path: "stateId",
+select: "",
+populate: {
+path: "zoneId",
+select: "",
+},
+},
+{
+path: "regionId",
+select: "",
+},
+{
+path: "beatId",
+select: "",
+},
+],
+},
+{
+path: "lineItems.product",
+select: "",
+},
+{
+path: "lineItems.price",
+select: "",
+},
+{
+path: "lineItems.inventoryId",
+select: "",
+},
+{ path: "billIds", select: "" },
+]);
 
-    if (!orderEntry) {
-      res.status(404);
-      throw new Error("Order Entry not found");
-    }
+if (!orderEntry) {
+  res.status(404);
+  throw new Error("Order Entry not found");
+}
 
-    const orderData = orderEntry.toObject();
+const orderData = orderEntry.toObject();
 
-    if (orderData.manualOrderDate) {
-      orderData.createdAt = orderData.manualOrderDate;
-    }
 
-    return res.status(200).json({
-      status: 200,
-      message: "Order Entry details retrieved successfully",
-      data: orderData,
-    });
-  } catch (error) {
-    res.status(400);
-    throw new Error(error?.message || "Something went wrong");
-  }
+if (orderData.salesmanName?._id) {
+const salesmanId = orderData.salesmanName._id;
+
+const salesmanRoute = await Beat.findOne({
+employeeId: salesmanId,
+status: true,
+});
+
+if (salesmanRoute) {
+orderData.routeId = salesmanRoute;
+}
+}
+
+if (orderData.manualOrderDate) {
+  orderData.createdAt = orderData.manualOrderDate;
+}
+
+return res.status(200).json({
+  status: 200,
+  message: "Order Entry details retrieved successfully",
+  data: orderData,
+});
+
+
+} catch (error) {
+res.status(400);
+throw new Error(error?.message || "Something went wrong");
+}
 }); // Get Order Entry Details by ID
 
-module.exports = { detailOrderEntry };
+module.exports = { detailOrderEntry }; 
